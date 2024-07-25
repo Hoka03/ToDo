cc7c23027ec1358ff217f598b603623373a1ce9b
HoKa <tolqinovoybek92@gmail.com>
2024-07-25 18:35:01 +0500

    swagger

--- config/urls.py
+++ config/urls.py
@@ -3,10 +3,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .yasg import schema_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todo.urls')),
+
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
