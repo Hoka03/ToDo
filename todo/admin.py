cee6c974824042b84b43bb31042e1d5f5aaa2e39
HoKa <tolqinovoybek92@gmail.com>
2024-07-25 17:57:49 +0500

    env

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from todo.models import ToDO
+
+
+@admin.register(ToDO)
+class ToDOAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'created_at')
+    list_display_links = list_display
